050b2c251b0dc9a3f774583be7f47a0fd8110071
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2021-08-11 10:45:28 +0200

    Rename method

--- src/HierarchicalStateInterface.php
+++ src/HierarchicalStateInterface.php
@@ -10,5 +10,5 @@ interface HierarchicalStateInterface extends NestedStateInterface
      * Specify which of the sub-states - if any - is active once this state becomes active
      * @see NestedStateInterface::children()
      */
-    public function initialSubState(): ?StateInterface;
+    public function initial(): ?StateInterface;
 }
