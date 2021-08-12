d4c9c2c15684e6025f3b2c5a1b1ff1532d4ce4c8
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2021-08-12 14:46:17 +0200

    Allow strings in StateInterface::equals

--- src/StateInterface.php
+++ src/StateInterface.php
@@ -6,15 +6,10 @@ interface StateInterface extends \Stringable
 {
 
     /**
-     * Compare with another state instance. This method is used to prevent ambiguity between
-     * string & instance comparisons It SHOULD be preferred over scalar string comparisons whenever possible
+     * Compare with another state instance. This method is used to prevent ambiguity when doing
+     * string & instance comparisons independently.
+     * It SHOULD be preferred over scalar string comparisons whenever possible
      */
-    public function equals(StateInterface $otherState): bool;
+    public function equals(string|self $otherState): bool;
 
-    /**
-     *  Event handlers might store metadata on the state object.
-     *  When this object is called, any such data MUST be cleared
-     * TODO: Think about replacing this with a getter for some StateContext class
-     */
-    //public function reset(): void;
 }
