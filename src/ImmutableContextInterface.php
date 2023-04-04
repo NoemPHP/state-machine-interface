05290eb1d49165c190a3e4dacc1f7e3624340d8c
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2023-04-04 20:20:58 +0200

    typo

--- src/ImmutableContextInterface.php
+++ src/ImmutableContextInterface.php
@@ -7,7 +7,7 @@ interface ImmutableContextInterface extends ContextInterface
 
     /**
      * Returns a new instance configured with a new trigger object.
-     * It is intended to be called ruing a transition to a new state, allowing context data to be retained
+     * It is intended to be called during a transition to a new state, allowing context data to be retained
      * while setting a new trigger
      *
      * @param object $trigger
